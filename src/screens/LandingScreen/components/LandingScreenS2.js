bf810cd2f57cfb0555d2f5a5731a98d62d18ee31
Shabd Bhatia <shabdbhatia@gmail.com>
2024-09-12 23:06:59 +0530

    Made event cards grid-like

--- src/screens/LandingScreen/components/LandingScreenS2.js
+++ src/screens/LandingScreen/components/LandingScreenS2.js
@@ -3,31 +3,31 @@ import React from "react";
 const LandingS2 = () => {
   const eventData = [
     {
-      title: "Event 1: Syndicalism",
+      title: "Syndicalism",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis purus sit amet magna gravida, vel tincidunt tortor suscipit. Duis ut gravida neque, ac posuere enim. Proin consectetur risus vel orci commodo, non tincidunt urna gravida. Morbi sed ex vel arcu suscipit eleifend. Integer tempor lorem id lorem elementum, id feugiat ipsum sodales. Donec ut neque vel felis feugiat maximus ac ut ligula. Vestibulum id nisi eu lorem tincidunt auctor. Nam facilisis ante id ligula rhoncus, non egestas libero vestibulum. Suspendisse potenti. Sed tincidunt erat at ante maximus, vel volutpat lectus facilisis.",
     },
     {
-      title: "Event 2: Explicatus",
+      title: "Explicatus",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis purus sit amet magna gravida, vel tincidunt tortor suscipit. Duis ut gravida neque, ac posuere enim. Proin consectetur risus vel orci commodo, non tincidunt urna gravida. Morbi sed ex vel arcu suscipit eleifend. Integer tempor lorem id lorem elementum, id feugiat ipsum sodales. Donec ut neque vel felis feugiat maximus ac ut ligula. Vestibulum id nisi eu lorem tincidunt auctor. Nam facilisis ante id ligula rhoncus, non egestas libero vestibulum. Suspendisse potenti. Sed tincidunt erat at ante maximus, vel volutpat lectus facilisis.",
     },
     {
-      title: "Event 3: The More the Money",
+      title: "The More the Money",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis purus sit amet magna gravida, vel tincidunt tortor suscipit. Duis ut gravida neque, ac posuere enim. Proin consectetur risus vel orci commodo, non tincidunt urna gravida. Morbi sed ex vel arcu suscipit eleifend. Integer tempor lorem id lorem elementum, id feugiat ipsum sodales. Donec ut neque vel felis feugiat maximus ac ut ligula. Vestibulum id nisi eu lorem tincidunt auctor. Nam facilisis ante id ligula rhoncus, non egestas libero vestibulum. Suspendisse potenti. Sed tincidunt erat at ante maximus, vel volutpat lectus facilisis.",
     },
     {
-      title: "Event 4: The Courting",
+      title: "The Courting",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis purus sit amet magna gravida, vel tincidunt tortor suscipit. Duis ut gravida neque, ac posuere enim. Proin consectetur risus vel orci commodo, non tincidunt urna gravida. Morbi sed ex vel arcu suscipit eleifend. Integer tempor lorem id lorem elementum, id feugiat ipsum sodales. Donec ut neque vel felis feugiat maximus ac ut ligula. Vestibulum id nisi eu lorem tincidunt auctor. Nam facilisis ante id ligula rhoncus, non egestas libero vestibulum. Suspendisse potenti. Sed tincidunt erat at ante maximus, vel volutpat lectus facilisis.",
     },
     {
-      title: "Event 5: Deal or No Deal",
+      title: "Deal or No Deal",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis purus sit amet magna gravida, vel tincidunt tortor suscipit. Duis ut gravida neque, ac posuere enim. Proin consectetur risus vel orci commodo, non tincidunt urna gravida. Morbi sed ex vel arcu suscipit eleifend. Integer tempor lorem id lorem elementum, id feugiat ipsum sodales. Donec ut neque vel felis feugiat maximus ac ut ligula. Vestibulum id nisi eu lorem tincidunt auctor. Nam facilisis ante id ligula rhoncus, non egestas libero vestibulum. Suspendisse potenti. Sed tincidunt erat at ante maximus, vel volutpat lectus facilisis.",
     },
     {
-      title: "Event 6: Trojan’s Trap",
+      title: "Trojan’s Trap",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis purus sit amet magna gravida, vel tincidunt tortor suscipit. Duis ut gravida neque, ac posuere enim. Proin consectetur risus vel orci commodo, non tincidunt urna gravida. Morbi sed ex vel arcu suscipit eleifend. Integer tempor lorem id lorem elementum, id feugiat ipsum sodales. Donec ut neque vel felis feugiat maximus ac ut ligula. Vestibulum id nisi eu lorem tincidunt auctor. Nam facilisis ante id ligula rhoncus, non egestas libero vestibulum. Suspendisse potenti. Sed tincidunt erat at ante maximus, vel volutpat lectus facilisis."
     },
     {
-        title: "Event 7: Ready Set Sell",
+        title: "Ready Set Sell",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis purus sit amet magna gravida, vel tincidunt tortor suscipit. Duis ut gravida neque, ac posuere enim. Proin consectetur risus vel orci commodo, non tincidunt urna gravida. Morbi sed ex vel arcu suscipit eleifend. Integer tempor lorem id lorem elementum, id feugiat ipsum sodales. Donec ut neque vel felis feugiat maximus ac ut ligula. Vestibulum id nisi eu lorem tincidunt auctor. Nam facilisis ante id ligula rhoncus, non egestas libero vestibulum. Suspendisse potenti. Sed tincidunt erat at ante maximus, vel volutpat lectus facilisis.",
       },
   ];
@@ -52,7 +52,7 @@ const LandingS2 = () => {
         {eventData.map((data) => (
           <div class="infocard-text-container" id="s2-event-card">
             <a href="#" id="s2-event-card-title">{data.title}</a>
-            <h2 id="s2-event-card-desc">{data.text}</h2>
+            {/* <h2 id="s2-event-card-desc">{data.text}</h2>  */}
           </div>
         ))}
       </div>
