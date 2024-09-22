c6c44a5b1d576bf3010eefd19c5a0b7d9963a445
adhirajgupta <60607630+adhirajgupta@users.noreply.github.com>
2024-09-22 22:35:48 +0530

    Faulty commit - used to resolve merge conflic
    Do not use this version to roll back website 
    will throw an error

--- src/eventData.js
+++ src/eventData.js
@@ -1,10 +1,10 @@
-import image1 from './screens/LandingScreen/Bkg-Images/DealNoDeal.png';
-import image2 from './screens/LandingScreen/Bkg-Images/Courting.png';
-import image3 from './screens/LandingScreen/Bkg-Images/Ready.png';
-import image4 from './screens/LandingScreen/Bkg-Images/Trojan.png';
-import image5 from './screens/LandingScreen/Bkg-Images/CapitalConquest.png';
-import image6 from './screens/LandingScreen/Bkg-Images/Clash.png';
-import image7 from './screens/LandingScreen/Bkg-Images/Deal.png';
+import dealNoDealImage from './screens/EventPage/DealNoDeal.png';
+import moreTheMoneyImage from './screens/EventPage/CapitalConquest.png';
+import readySetSellImage from './screens/EventPage/Ready.png';
+import trojansTankImage from './screens/EventPage/Trojan.png';
+import theCourtingImage from './screens/EventPage/Courting.png';
+import conquerTheCrisisImage from './screens/EventPage/Clash.png';
+import stakeholdersShowdownImage from './screens/EventPage/Deal.png';
 
 const eventData = [
   {
@@ -14,45 +14,44 @@ const eventData = [
     description: "Teamwork makes the dream work! Get ready for TISB's Champions League auction, where you’ll draft top players within your budget. With high stakes, strategy is key. Build the ultimate team, outsmart rivals, and aim for victory. Play smart, lead your squad, and rise to the top. The championship is yours for the taking!",
     rules: "Rules for Event 1",
     prizes: "Prizes for event 1",
-    image: image1,
+    bgimage: dealNoDealImage,
   },
   {
     id: 2,
-    title: "The Courting",
-    tagline: "The ball is in your court",
-    description: "In three rounds of fierce debate, you’ll be handed a legal case brief and be tasked to plead a side. The competition is intense, but there are remarkably greater rewards at stake. So, do you think you have what it takes? Well, if you’ve got the analytical thinking skills and the persuasive ability, the ball is in your court and the victors of The Courting are on your team.",
-    rules: "rules######",
-    prizes: "prizes######",
-    image: image2,
-  },
-  {
-    id: 3,
-    title: "Ready? Set? Sell!",
-    tagline: "From Mind to Media, Lead the Idea!",
-    description: "Get ready to ignite your creativity and showcase your marketing prowess at Ready? Set. Sell!! This is the ultimate arena where strategy meets innovation. Compete in thrilling rounds that challenge your advertising skills, push your boundaries, and let your ideas shine. Gather your team, embrace the excitement, and prove you have what it takes to lead the idea. The spotlight is yours—are you ready to sell?",
-    rules: "rules######",
-    prizes: "prizes######",
-    image: image3,
-  },
-  {
-    id: 4,
-    title: "Trojan's Tank",
-    tagline: "Sink or Swim",
-    description: "Trojans tank will be a replica of Shark Tank wherein participants will be pre-assigned a industry and the participants will be tasked to create an innovative pitch for a unique product in order to raise money for investment purposes. HOD’s will be in charge of grading and scoring the pitch depending on how unique and how applicable it is to our current real life scenarios.",
-    rules: "rules######",
-    prizes: "prizes######",
-    image: image4,
-  },
-  {
-    id: 5,
     title: "The More the Money",
     tagline: "High stakes, high rewards",
     description: "Get ready for an exciting and challenging quiz event where knowledge from four diverse categories—current affairs, economics, finance, and history—will be put to the test. Teams will compete through multiple rounds, each with a unique format, designed to push their knowledge, quick thinking, and strategy to the limit. With high stakes and a competitive edge, the best teams will rise through the ranks to claim victory!",
     rules: "rules######",
     prizes: "prizes######",
-    image: image5,
+    bgimage: moreTheMoneyImage,
   },
-  
+  {
+    id: 3,
+    title: "Ready? Set? Sell!",
+    tagline: "Sink or Swim",
+    description: "Get ready to ignite your creativity and showcase your marketing prowess at Ready? Set. Sell!! This is the ultimate arena where strategy meets innovation. Compete in thrilling rounds that challenge your advertising skills, push your boundaries, and let your ideas shine. Gather your team, embrace the excitement, and prove you have what it takes to lead the idea. The spotlight is yours—are you ready to sell?",
+    rules: "rules######",
+    prizes: "prizes######",
+    bgimage: readySetSellImage,
+  },
+  {
+    id: 4,
+    title: "Trojans Tank",
+    tagline: "Sink or Swim",
+    description: "Trojans tank will be a replica of Shark Tank wherein participants will be pre-assigned a industry and the participants will be tasked to create an innovative pitch for a unique product in order to raise money for investment purposes. HOD’s will be in charge of grading and scoring the pitch depending on how unique and how applicable it is to our current real life scenarios. ",
+    rules: "rules######",
+    prizes: "prizes######",
+    bgimage: trojansTankImage,
+  },
+  {
+    id: 5,
+    title: "The Courting",
+    tagline: "The ball is in your court",
+    description: "In three rounds of fierce debate, you’ll be handed a legal case brief and be tasked to plead a side. The competition is intense, but there are remarkably greater rewards at stake. So, do you think you have what it takes? Well, if you’ve got the analytical thinking skills and the persuasive ability, the ball is in your court and the victors of The Courting are on your team.",
+    rules: "rules######",
+    prizes: "prizes######",
+    bgimage: theCourtingImage,
+  },
   {
     id: 6,
     title: "Conquer the Crisis",
@@ -60,7 +59,7 @@ const eventData = [
     description: "Conquer The Crisis is an event designed for participants interested in diplomacy and economic policy. It challenges participants to handle crises as finance ministers of assigned countries, requiring them to devise solutions. This event will test Presentation and Negotiation skills along with theoretical economic knowledge.",
     rules: "rules######",
     prizes: "prizes######",
-    image: image6,
+    bgimage: conquerTheCrisisImage,
   },
   {
     id: 7,
@@ -69,7 +68,7 @@ const eventData = [
     description: "Join the Stakeholder’s showdown, where you represent a key faction in a collapsing economy. With limited resources and mounting pressure, you must negotiate, strategize, and advocate to guide your group through financial turmoil. Build alliances, make tough decisions, and fight for stability amidst uncertainty. Your goal? Secure success and survival for your faction in a volatile landscape where every choice impacts the future. Can you lead through the crisis?",
     rules: "rules######",
     prizes: "prizes######",
-    image: image7,
+    bgimage: stakeholdersShowdownImage,
   },
   // Add other events here
 ];
