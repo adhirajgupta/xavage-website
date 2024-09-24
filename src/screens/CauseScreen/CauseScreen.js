67004f57eaf1b01a53014dc0873a82c13f96b5b5
Gautham Gopinath <gautham171206@gmail.com>
2024-09-24 18:32:49 +0530

    finished causescreen section 2

--- src/screens/CauseScreen/CauseScreen.js
+++ src/screens/CauseScreen/CauseScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import Footer from './../LandingScreen/components/LandingScreenS4';
 import CauseS1 from "./components/CauseScreenS1";
-// import CauseS2 from "./components/CauseScreenS2"
+import CauseS2 from "./components/CauseScreenS2"
 // import LandingS3 from "./components/LandingScreenS3";
 // import LandingS4 from "./components/LandingScreenS4";
 
 
 import './CauseScreenStyling/CauseScreenS1Style.css';
-// import './CauseScreenStyling/CauseScreenS2Style.css';
+import './CauseScreenStyling/CauseScreenS2Style.css';
 // import './LandingScreenStyling/LandingScreenS3Style.css';
 // import './LandingScreenStyling/LandingScreenS4Style.css';
 // <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"></link>
@@ -16,6 +17,8 @@ const Cause = () => {
   return (
     <div>
         <CauseS1/>
+        <CauseS2/>
+        <Footer/>
     </div>
   );
 };
--- src/screens/CauseScreen/components/CauseScreenS1.js
+++ src/screens/CauseScreen/components/CauseScreenS1.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from '../images/Xavage_2024_Sponsorship_Proposal.png'
 
 const CauseS1 = () => {
   
@@ -9,8 +8,7 @@ const CauseS1 = () => {
             <h1>WHY WE HOST XAVAGE?</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus fringilla velit at luctus. Pellentesque mauris sem, pharetra nec venenatis semper, mattis id mauris. Mauris sed sodales dolor, sed tristique nunc. Vivamus in quam molestie, porta neque sit amet, tempus lacus. Maecenas ut pretium mi, nec commodo magna. Sed ultricies, turpis at aliquam cursus, ipsum libero pretium magna, non tristique eros nulla at orci. Quisque scelerisque quam ut felis blandit pulvinar a at ligula.</p>
         </div>
-        {/* <image src={logo} id="logo" alt="LOGO"/> */}
-        <section id="logo"><p>LOGO</p></section>
+        <img src={require('./xavage-24-logo.png')} id="logo" alt="LOGO"/>
     </div>
   );
 };
