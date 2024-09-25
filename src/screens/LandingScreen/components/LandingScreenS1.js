787e6b9ab46f30c652b1fc9b3fabcb1b3d330670
Shabd Bhatia <shabdbhatia@gmail.com>
2024-09-26 00:03:19 +0530

    Changed font and button colour

--- src/screens/LandingScreen/components/LandingScreenS1.js
+++ src/screens/LandingScreen/components/LandingScreenS1.js
@@ -1,22 +1,21 @@
 import React from "react";
 
 const LandingS1 = () => {
-  
   return (
     <div class="container" id="landing-s1-container">
-        <div class="container" id="landing-s1-inner-container">
-            {/* <div class="img-container" id="landing-s1-logo-div">
+      <div class="container" id="landing-s1-inner-container">
+        {/* <div class="img-container" id="landing-s1-logo-div">
                 <img src="/src/screens/LandingScreen/logo-placeholder-image.png" alt="xavage-logo"/>
             </div> */}
 
-            <div class="registration-container" id="landing-s1-registration-div">
-                <h1 id="xavage-heading">XAVAGE 2024</h1>
-                <h1 id="edition-heading">10th Edition</h1>
-                <button class="primary-button" id="registration-button">
-                    Register Now!
-                </button>
-            </div>
+        <div class="registration-container" id="landing-s1-registration-div">
+          <h1 id="xavage-heading">XAVAGE 2024</h1>
+          <h1 id="edition-heading">10th Edition</h1>
         </div>
+      </div>
+      <button class="primary-button" id="registration-button">
+        Register Now!
+      </button>
     </div>
   );
 };
